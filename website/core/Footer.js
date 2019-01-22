a7fe04ed14e3a6f4bca0421523b861e818453ab1
Anastasia Kashkinova <anastasia.kashkinova@gmail.com>
2019-01-22 13:40:33 +0900

    Update footer

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -50,26 +50,19 @@ class Footer extends React.Component {
             </a>
           </div>
           <div>
-            <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>
-              User Showcase
+            <h5>Sider</h5>
+            <a href="https://sider.review/">
+              Sider Top
             </a>
-            <a
-              href="http://stackoverflow.com/questions/tagged/"
-              target="_blank"
-              rel="noreferrer noopener">
-              Stack Overflow
+            <a href="https://sider.review/terms">
+              Terms of Service
             </a>
-            <a href="https://discordapp.com/">Project Chat</a>
-            <a
-              href="https://twitter.com/"
-              target="_blank"
-              rel="noreferrer noopener">
-              Twitter
+            <a href="https://sider.review/">
+              Privacy
             </a>
           </div>
           <div>
-            <h5>More</h5>
+            <h5>Social</h5>
             <a href="https://blog.sideci.com">Blog</a>
             <a href="https://github.com/sider">GitHub</a>
             <a
